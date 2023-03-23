1b4845a6cb9507c60eadec84feed746cab9e5eab
piyumanthiNisansala <95172003+piyumanthiNisansala@users.noreply.github.com>
2023-03-23 15:26:22 +0530

    Create Login.js

---
+++ src/routes/Login.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { BsNewspaper } from "react-icons/bs";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Button from "@mui/material/Button";
+import { Password } from "@mui/icons-material";
+import { margin } from "@mui/system";
+
+function Login() {
+
+    const paperStyle = { padding: 20, height: '60vh', width: 300, margin: "20px auto" }
+    const btnStyle = { margin: '8px 0' }
+    
+  
+  return (
+    <div>
+    <div className="login">
+          <h1>Login</h1>
+          <BsNewspaper className="page-icon" />
+        </div>
+        <Grid container justifyContent="center" alignItems="center" style={{ height: "70vh" }}>
+      <Paper elevation={10} style={paperStyle}>
+      <h2 style={{ textAlign: "center" }}>Sign In</h2>&nbsp;
+        <TextField label='Username' placeholder="Enter username" fullWidth required />&nbsp;
+        <TextField label='Password' placeholder="Enter password" type='password' fullWidth required />&nbsp;
+        <Button type='Submit' color='primary' variant="contained" style={btnStyle} fullWidth>Sign in</Button>&nbsp;
+        <Typography>
+          <Link href="#" >
+            Forgot Password ?
+          </Link>
+        </Typography>&nbsp;
+        <Typography> Not Registered ?
+          <Link href="#" >
+            Create an Account
+          </Link>
+        </Typography>
+      </Paper>
+    </Grid>
+    </div>
+
+  )
+}
+
+export default Login
