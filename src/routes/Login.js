f27f863e868e1bd2a39a8957c377e5f0d0539b39
piyumanthiNisansala <95172003+piyumanthiNisansala@users.noreply.github.com>
2023-03-24 08:33:41 +0530

    Updated Login.js

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -13,33 +13,33 @@ function Login() {
 
     const paperStyle = { padding: 20, height: '60vh', width: 300, margin: "20px auto" }
     const btnStyle = { margin: '8px 0' }
-    
-  
-  return (
-    <div>
-    {/*<div className="login">
+
+
+    return (
+        <div>
+            {/*<div className="login">
           <h1>Login</h1>
           <BsNewspaper className="page-icon" />
   </div>*/}
-        <Grid container justifyContent="center" alignItems="center" style={{ height: "100vh" }}>
-      <Paper elevation={10} style={paperStyle}>
-      <h2 style={{ textAlign: "center" }}>Sign In</h2>&nbsp;
-        <TextField label='Username' placeholder="Enter username" fullWidth required />&nbsp;
-        <TextField label='Password' placeholder="Enter password" type='password' fullWidth required />&nbsp;
-        <Button type='Submit' color='primary' variant="contained" style={btnStyle} fullWidth>Sign in</Button>&nbsp;
-        <Typography>
-        <Link to="/PasswordResetForm">Forgot Password?</Link>
-        </Typography>&nbsp;
-        <Typography> Not Registered?&nbsp;
-          <Link href="#" >
-            Create an Account
-          </Link>
-        </Typography>
-      </Paper>
-    </Grid>
-    </div>
+            <Grid container justifyContent="center" alignItems="center" style={{ height: "100vh" }}>
+                <Paper elevation={10} style={paperStyle}>
+                    <h2 style={{ textAlign: "center" }}>Sign In</h2>&nbsp;
+                    <TextField label='Username' placeholder="Enter username" fullWidth required />&nbsp;
+                    <TextField label='Password' placeholder="Enter password" type='password' fullWidth required />&nbsp;
+                    <Button type='Submit' color='primary' variant="contained" style={btnStyle} fullWidth component={Link} to="/">Sign in</Button>&nbsp;
+                    <Typography>
+                        <Link to="/PasswordResetForm">Forgot Password?</Link>
+                    </Typography>&nbsp;
+                    <Typography> Not Registered?&nbsp;
+                        <Link href="#" >
+                            Create an Account
+                        </Link>
+                    </Typography>
+                </Paper>
+            </Grid>
+        </div>
 
-  )
+    )
 }
 
 export default Login
