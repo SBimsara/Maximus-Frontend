fdd77981b44d143783cc186e1c59fb5c0cf61ee0
piyumanthiNisansala <95172003+piyumanthiNisansala@users.noreply.github.com>
2023-03-24 08:34:32 +0530

    Updated PasswordResetForm.js

--- src/routes/PasswordResetForm.js
+++ src/routes/PasswordResetForm.js
@@ -9,25 +9,25 @@ import { margin } from "@mui/system";
 
 function PasswordResetForm() {
 
-  const paperStyle = { padding: 20, height: '50vh', width: 300, margin: "20px auto" }
-  const btnStyle = { margin: '8px 0' }
-    
-  return (
-    <div>
-      {/*<div className="passwordResetForm">
+    const paperStyle = { padding: 20, height: '50vh', width: 300, margin: "20px auto" }
+    const btnStyle = { margin: '8px 0' }
+
+    return (
+        <div>
+            {/*<div className="passwordResetForm">
           <h1>PasswordResetForm</h1>
           <BsNewspaper className="page-icon" />
       </div>*/}
-      <Grid container justifyContent="center" alignItems="center" style={{ height: "100vh" }}>
-        <Paper elevation={10} style={paperStyle}>
-          <h2 style={{ textAlign: "center" }}>Reset Password</h2>&nbsp;
-          <TextField label='New Password' placeholder="Enter new password" fullWidth required />&nbsp;
-          <TextField label='Confirm Password' placeholder="Enter new password again" type='password' fullWidth required />&nbsp;
-          <Button type='Submit' color='primary' variant="contained" style={btnStyle} fullWidth>Reset Password</Button>&nbsp;
-        </Paper>
-      </Grid>
-    </div>
-  )
+            <Grid container justifyContent="center" alignItems="center" style={{ height: "100vh" }}>
+                <Paper elevation={10} style={paperStyle}>
+                    <h2 style={{ textAlign: "center" }}>Reset Password</h2>&nbsp;
+                    <TextField label='New Password' placeholder="Enter new password" fullWidth required />&nbsp;
+                    <TextField label='Confirm Password' placeholder="Enter new password again" type='password' fullWidth required />&nbsp;
+                    <Button type='Submit' color='primary' variant="contained" style={btnStyle} fullWidth>Reset Password</Button>&nbsp;
+                </Paper>
+            </Grid>
+        </div>
+    )
 }
 
 export default PasswordResetForm;
