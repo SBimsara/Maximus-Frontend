07aa23c6f568bdf5c80c05bf3f0967f2003336e3
Pehan Sanchitha <sanchithaspyp.20@uom.lk>
2023-03-27 05:09:21 +0530

    Stat1 chart connected with Backend API
    Stat1 chart previously worked with raw data. Therefore,  connect the Stat1 chart with backend subscriptionStat API using axios.

--- src/routes/Statistics/Stat1/Stat1.js
+++ src/routes/Statistics/Stat1/Stat1.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as chartjs, ArcElement, Tooltip, Legend } from "chart.js";
 import Nav from "../navBar/Nav";
+import axios from "axios";
 
 chartjs.register(ArcElement, Tooltip, Legend);
 
 function Stat1() {
+  const [chartData, setchartData] = useState({});
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/api/v1/subscriptionStat/getPlanUserCount"
+        );
+        console.log(response);
+        setchartData({
+          labels: ["Free-Plan", "Bronze-Plan", "Silver-Plan", "Gold-Plan"],
+          data: response.data.map((item) => item),
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
   const data = {
-    labels: ["Subscribed", "Unsubscribed"],
+    labels: chartData.labels,
     datasets: [
       {
-        data: [80, 20],
-        backgroundColor: ["black", "orange"],
+        data: chartData.data,
+        backgroundColor: ["green", "brown", "silver", "yellow"],
       },
     ],
   };
@@ -22,16 +42,13 @@ function Stat1() {
     },
   };
   return (
-    <div>
-      <Nav></Nav>
-      <div
-        style={{
-          width: "45%",
-          margin: "0 auto",
-        }}
-      >
-        <Pie data={data} options={options}></Pie>
-      </div>
+    <div
+      style={{
+        width: "45%",
+        margin: "0 auto",
+      }}
+    >
+      <Pie data={data} options={options}></Pie>
     </div>
   );
 }
