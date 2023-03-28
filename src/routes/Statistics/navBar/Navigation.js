7703e51e94888f65981d818177452af3b402cffe
Pehan Sanchitha <sanchithaspyp.20@uom.lk>
2023-03-29 04:43:02 +0530

    Add dropdown button to Navigation bar in statistics page
    In the Navigation bar which is only visible in statistics page, remove the nav items and add one dropdown button from bootstrap and add navigation to stat0 & stat1 when selecting from dropdown button

--- src/routes/Statistics/navBar/Navigation.js
+++ src/routes/Statistics/navBar/Navigation.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Navigation() {
   const location = useLocation();
+
+  const [selectedOption, setSelectedOption] = useState("Stat0");
+
+  const handleOptionSelect = (eventKey) => {
+    setSelectedOption(eventKey);
+  };
+
+  let navigate = useNavigate();
 
   if (
     location.pathname === "/statistics" ||
@@ -17,17 +25,40 @@ function Navigation() {
         expand="lg"
         style={{ paddingTop: "60px", paddingLeft: "238px" }}
       >
-        {/* <Navbar.Brand href="/statistics">My Website</Navbar.Brand> */}
+        <Navbar.Brand style={{ paddingLeft: "8px" }}>
+          Select an Item
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          {/* <Nav className="mr-auto">
             <LinkContainer to="/statistics/Stat0">
               <Nav.Link>Stat0</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/statistics/Stat1">
               <Nav.Link>Stat1</Nav.Link>
             </LinkContainer>
-          </Nav>
+          </Nav> */}
+          <NavDropdown
+            className="btn btn-lg "
+            title={selectedOption}
+            id="basic-nav-dropdown"
+            onSelect={handleOptionSelect}
+          >
+            <NavDropdown.Item
+              eventKey="Stat0"
+              href="/statistics/Stat0"
+              // onClick={() => navigate("/statistics/Stat0")}
+            >
+              Stat0
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              eventKey="Stat1"
+              href="/statistics/Stat1"
+              onClick={() => navigate("/statistics/Stat0")}
+            >
+              Stat1
+            </NavDropdown.Item>
+          </NavDropdown>
         </Navbar.Collapse>
       </Navbar>
     );
