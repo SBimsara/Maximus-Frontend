5547d705ba47d9468c84d282cac714a9bacf89fa
piyumanthiNisansala <95172003+piyumanthiNisansala@users.noreply.github.com>
2023-06-09 15:39:41 +0530

    connected the frontend and backend

--- src/routes/LoginForm/Login.js
+++ src/routes/LoginForm/Login.js
@@ -8,11 +8,56 @@ import Button from "@mui/material/Button";
 import { Link } from 'react-router-dom';
 // Importing custom styles.
 import './Styles/LoginStyles.css';
+import axios from 'axios';
+import { useState } from 'react';
+import Dashboard from "../Dashboard";
+
 
 function Login() {
   // Setting up paper and button styles.
   const paperStyle = { padding: 20, height: '60vh', minwidth: '300px', width: '70%', maxwidth: '500px', margin: "20px auto", borderRadius: 20 };
   const btnStyle = { margin: '8px 0' };
+
+  const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [usernameError, setUsernameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false); // State to track login status
+
+  const handleLogin = async () => {
+
+    // Resetting any previous error messages
+    setUsernameError('');
+    setPasswordError('');
+
+    // Validating the username and password
+    if (!username) {
+      setUsernameError('Please enter a username');
+      return;
+    }
+
+    if (!password) {
+      setPasswordError('Please enter a password');
+      return;
+    }
+
+    // Create an object with the user's information.
+    const data = {
+      "username": username,
+      "password": password,
+    };
+  }
+
+  async function loginAPI() {
+  try {
+    const response = await axios.get('http://localhost:8080/api/v1/admin/login');
+    console.log(response); // Do something with the response
+     // Assuming the login API returns a success status
+     setLoggedIn(true); // Set the login status to true
+  } catch (error) {
+    console.error(error); // Handle any errors that occurred during the API call
+  }
+};
 
   // Rendering Login component.
   return (
@@ -36,7 +81,7 @@ function Login() {
                 <TextField label='Username' placeholder="Enter username" fullWidth required={true} style={{ marginTop: 8 }} />&nbsp;
                 <TextField label='Password' placeholder="Enter password" type='password' fullWidth required={true} style={{ marginTop: 8 }} />&nbsp;
                 {/* Sign in button */}
-                <Button type='Submit' color='primary' variant="contained" style={btnStyle} fullWidth component={Link} to="/">Sign in</Button>&nbsp;
+                <Button type='Submit' color='primary' variant="contained" style={btnStyle} fullWidth onClick={handleLogin}>Sign in</Button> {loggedIn && <Dashboard/>} {/* Render the Dashboard component if logged in */} &nbsp;
                 {/* Forgot password link. */}
                 <Typography>
                   <Link to="/PasswordResetForm" className="login-link">Forgot Password?</Link>
