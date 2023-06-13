b81c91a12bc7ca0c68042e27e034f02582648e8b
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-06-13 17:23:12 +0530

    changed App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ import Stat1 from "./routes/Statistics/Stat1/Stat1";
 import Stat0 from "./routes/Statistics/Stat0/Stat0";
 import Nav from "./routes/Statistics/navBar/Nav";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Popup from "./routes/Plans/Popup";
+
 import AddPlans from "./routes/Plans/AddPlans";
 import PlanDetails from "./routes/Plans/PlanDetails";
-import './App.css';
-import Sidebar  from "./components/Sidebar";
-import Navbar  from "./components/Navbar";
-
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 import PageLayout from "./routes/PageLayout";
 import Settings from "./routes/Settings";
@@ -27,7 +26,6 @@ import CustomDropdown from "./components/form/CustomDropdown";
 import PlanPopup from "./routes/Plans/PlanPopup";
 import ActionAlerts from "./components/ui/actionAlerts";
 
-
 function App() {
   return (
     <>
@@ -36,12 +34,12 @@ function App() {
       {/* <Navbar /> */}
       <Navigation />
       <Routes>
-        <Route path="/" element={<Dashboard/>} />
-        <Route path="/questions" element={<Questions/>} />
-        <Route path="/subjects" element={<Subjects/>} />
-        <Route path="/addPlans" element={<AddPlans/>}/>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/questions" element={<Questions />} />
+        <Route path="/subjects" element={<Subjects />} />
+        <Route path="/addPlans" element={<AddPlans />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/planDe" element={<PlanDetails/>}/>
+        <Route path="/planDe" element={<PlanDetails />} />
         <Route path="/statistics" element={<Statistics />} />
         <Route path="/statistics/Stat1" element={<Stat1 />} exact />
         <Route path="/statistics/Stat0" element={<Stat0 />} />
